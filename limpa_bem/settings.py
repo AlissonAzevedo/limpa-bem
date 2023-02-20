9681923a6725b7e711331d31399d4c720c6223b2
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2023-02-20 12:35:31 -0300

    deploy: add url railway in CSRF_TRUSTED_ORIGINS

--- limpa_bem/settings.py
+++ limpa_bem/settings.py
@@ -171,5 +171,5 @@ CORS_ALLOW_METHODS = [
 ]
 
 APPEND_SLASH = False
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+CSRF_TRUSTED_ORIGINS = ["web-production-512c.up.railway.app", "127.0.0.1"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
